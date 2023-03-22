d31171f81fbf86be71ac101d93b345ca41b9f9bd
cristiancv <cristiancv100@gmail.com>
2023-03-22 15:31:03 -0500

    Initial commit

---
+++ js/index_dom.js
@@ -0,0 +1,6 @@
+import hamburgerMenu from '../dom/menu_hamburguesa.js';
+//hamburgerMenu(".panel-btn",".panel",".menu a");
+const d=document;
+d.addEventListener("DOMContentLoaded", (e)=>{
+  hamburgerMenu(".panel-btn",".panel", ".menu a");
+});
